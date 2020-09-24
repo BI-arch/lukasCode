b88c8651fb4a4938545046928f60acd88c7c90fb
BI-arch <lukasz.kolakowski@biarch.pl>
2020-09-24 15:20:44 +0200

    test

---
+++ public_bills/app.js
@@ -0,0 +1,198 @@
+document.querySelector(".qty").addEventListener("change", calcVal)
+document.querySelector(".prc").addEventListener("change", calcVal)
+document.querySelector("input[list=vendors]").addEventListener("keyup", checkValue)
+
+function checkValue(keyup) {
+  let valLenght = keyup.target.value.length
+  if (valLenght == keyup.target.maxLength ) {
+    keyup.target.className = "maxLen"
+  }
+  else { keyup.target.className = "" }
+}
+
+fetch('/getVendors')
+    .then(res => res.json())
+    .then(vendors => {addVendorsToList(vendors)})
+
+
+fetch('/getProducts')
+    .then(res => res.json())
+    .then(products => {addProductsToList(products)})
+
+function calcVal(ev) {
+    //console.log(ev.target.className)
+    let val = ev.target.parentElement.parentElement.querySelector(".val")
+    let qty = ev.target.parentElement.parentElement.querySelector(".qty")
+    let prc = ev.target.parentElement.parentElement.querySelector(".prc")
+    // ev.target.parentElement.parentElement.querySelector(".qty").value = formatNumber(qty.value)
+    // ev.target.parentElement.parentElement.querySelector(".prc").value = formatNumber(prc.value)
+    val.textContent = formatNumber(qty.value * prc.value)
+    recalcSum()
+}
+
+document.querySelector(".addpos").addEventListener("click", addNewRow)
+
+function addNewRow(click){
+    let button = click.target
+    let tr = button.parentElement.parentElement
+    let clonedTr = tr.cloneNode(true)
+
+    //tu czysci input
+    clonedTr.querySelectorAll("input").forEach(input => {input.value=""})
+    clonedTr.querySelector(".val").textContent =""
+    let newQty = clonedTr.querySelector(".qty")
+    let newPrc = clonedTr.querySelector(".prc")
+    //przypisanie funkcji calcVal do inputow w sklonowanym wierszu
+    newQty.addEventListener("change", calcVal)
+    newPrc.addEventListener("change", calcVal)
+    //kasujemy guzik
+    addDelButton(button)
+    clonedTr.querySelector(".addpos").addEventListener("click", addNewRow)
+    let sum = tr.parentElement.querySelector(".sum")
+    sum = tr.parentElement.removeChild(sum)
+    tr.parentElement.appendChild(clonedTr)
+    tr.parentElement.appendChild(sum)
+    recalcLp()
+}
+
+function addDelButton(oldButton){
+    oldButton.textContent = "Usun"
+    oldButton.removeEventListener("click", addNewRow)
+    oldButton.addEventListener("click", removeRow)
+}
+
+function removeRow(click){
+    let tr = click.target.parentElement.parentElement
+    tr.parentElement.removeChild(tr)
+    recalcLp()
+    recalcSum()
+}
+
+function recalcLp(){
+    let lps = document.querySelectorAll(".lp")
+    //console.log(lps), iniciowanie petli, kontrola powtorzen, zwiekszanie)
+    for (let i =0;lps.length >i;i=i+1) {
+        lps[i].textContent = i+1
+        //toggle - przelacza klase. jak jest usuwa jak nie ma to dodaje
+        if(i%2) lps[i].parentElement.className = "second" 
+        else lps[i].parentElement.className = "" 
+    }
+}
+
+//document.querySelectorAll(".val")
+//document.getElementById("val1")
+function recalcSum(){
+    let vals = document.querySelectorAll(".val"), sumVal = 0;
+    for (let i = 0; i < vals.length;i++)
+    {
+        sumVal += Number(vals[i].textContent.replace(/,/g,''))
+        //sumVal = sumVal+querySelector(".val")[i];
+    }
+    document.getElementById("sum").textContent = formatNumber(sumVal)
+}
+
+function formatNumber(number) {
+    return Number(Number(number).toFixed(2)).toLocaleString("en",{minimumFractionDigits: 2});
+}
+
+var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth()+1; 
+var yyyy = today.getFullYear();
+ if(dd<10){
+        dd='0'+dd
+    } 
+    if(mm<10){
+        mm='0'+mm
+    } 
+
+today = yyyy+'-'+mm+'-'+dd;
+document.getElementById("datefield").setAttribute("max", today)
+
+//odpowiedzialne za miejsca dziesietne i separatory we wszystkich liczbch i kalkulacjach
+function addCommas(nStr)
+{
+    nStr += '';
+    x = nStr.split('.');
+    x1 = x[0];
+    x2 = x.length > 1 ? '.' + x[1] : '';
+    var rgx = /(\d+)(\d{3})/;
+    while (rgx.test(x1)) {
+        x1 = x1.replace(rgx, '$1' + ',' + '$2');
+    }
+    return x1 + x2;
+}
+
+
+const openModalButtons = document.querySelectorAll('[data-modal-target]')
+const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay')
+
+openModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    // const modal = document.querySelector(button.dataset.modalTarget)
+    // openModal(modal)
+    let newVendor = document.querySelector("input[list=vendors]").value
+    fetch("/addVendor",{method: 'POST', body: newVendor})    
+      .then(res => res.text())
+      .then(message => {alert(message)})
+  })
+})
+
+overlay.addEventListener('click', () => {
+  const modals = document.querySelectorAll('.modal.active')
+  modals.forEach(modal => {
+    closeModal(modal)
+  })
+})
+
+closeModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = button.closest('.modal')
+    closeModal(modal)
+  })
+})
+
+function openModal(modal) {
+  if (modal == null) return
+  modal.classList.add('active')
+  overlay.classList.add('active')
+}
+
+function closeModal(modal) {
+  if (modal == null) return
+  modal.classList.remove('active')
+  overlay.classList.remove('active')
+}
+
+function addVendorsToList(vendorsJson) {
+    
+    vendorsJson.forEach(vendor => {
+        let newVendor = document.createElement("OPTION")
+        newVendor.value = vendor.vendorName
+        document.getElementById("vendors").appendChild(newVendor)        
+    })
+
+    // for (let i = 0; i < vendorsJson.data.length;i++){
+    //     let newVendor = document.createElement("OPTION")
+    //     newVendor.value = vendorsJson.data[i]
+    //     document.getElementById("vendors").appendChild(newVendor)
+    // }   
+}
+
+
+
+function addProductsToList(productsJson){
+    
+  productsJson.forEach(product => {
+      let newProduct = document.createElement("OPTION")
+      newProduct.value = product.productName
+      document.getElementById("products").appendChild(newProduct)        
+  })
+
+  // for (let i = 0; i < vendorsJson.data.length;i++){
+  //     let newVendor = document.createElement("OPTION")
+  //     newVendor.value = vendorsJson.data[i]
+  //     document.getElementById("vendors").appendChild(newVendor)
+  // }   
+}
---
+++ server.js
@@ -0,0 +1,82 @@
+const server = require("connect")();
+const static = require("serve-static");
+const port = 80
+const MSSQL = require('mssql')
+const body = require('body-parser')
+
+const pool = new MSSQL.ConnectionPool({
+    user: 'biadmin',
+    password: 'MiSt15Wa96*!',
+    server: 'bisqlstd.westeurope.cloudapp.azure.com',
+    database: 'BI001_CST'
+})
+
+pool.connect(err => {
+    if(err) {
+        console.log('Error: ', err)
+      }
+      // If no error, then good to go...
+      else {
+          console.log("sql connected succesfully")
+      }
+})
+
+//log details every request
+server.use((req, res, next) => {
+    console.log("request received on " + new Date())
+    console.log("request received from " + req.headers.host + req.url)
+    next()
+})
+
+server.use(body.text())
+
+//serves content of public_luk if url request is /home
+server.use("/home",static("public_luk"))
+
+//serves content of public_bills if url request is /
+server.use("/",static("public_bills"))
+
+//Display vendors list for path /getVendors
+server.use("/getVendors", getVendors)
+
+//Display products list for path /products
+server.use("/getProducts", getProducts)
+
+//Add vendors to data base
+server.use("/addVendor", addVendor)
+
+//response Fack You for all other urls
+server.use((req, res) => {
+    res.end("Fuck You, there is no " + req.url)
+})
+
+server.listen(port)
+console.log("server listening on port: " + port )
+
+async function getVendors(req, res) {
+    let query = "Select vendorName from dim.Vendor "
+    const result = await pool.query(query)
+    res.end(JSON.stringify(result.recordset))
+}
+
+async function getProducts(req, res) {
+    let query = "Select productName from dim.Product "
+    const result = await pool.query(query)
+    res.end(JSON.stringify(result.recordset))
+}
+
+
+async function addVendor(req, res) {
+    let newVendor = req.body
+    let query = `Insert into dim.Vendor values('${newVendor}')`
+    try {
+        const result = await pool.query(query)
+        console.log(result)
+        res.end(`dodano ${newVendor}`)
+    }
+    catch (err) {
+        console.log(err)
+        res.end(err.message)
+    }
+
+}
